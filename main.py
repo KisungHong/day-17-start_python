ffd9eff36c0aed15a5ca909f7e91c3bfabafe44f
kisung <ppter0311@naver.com>
2023-02-02 12:02:10 +0900

    class_constructor

---
+++ main.py
@@ -0,0 +1,36 @@
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+
+
+user_1 = User("001", "angela")
+user_2 = User("002", "jack")
+print(user_1.id)
+print(user_1.username)
+
+user_1.follow(user_2)
+
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
+
+# user1 = User()
+# user1.id = "001"
+# user1.username = "angela"
+#
+# print(user1.username)
+#
+# user2 = User()
+# user2.id = "002"
+# user2.username = "kisung"
+#
+# print(user2.username)
